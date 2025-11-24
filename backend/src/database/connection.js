57e4b61a557aa26488a5e0066ca09e7700c2f0fd
GiovanniAzevedo_DEV <giovanniazevedo065@gmail.com>
2025-11-24 17:51:57 -0300

    alterei o db

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "", // coloque sua senha se tiver
+  password: "08111999", 
   database: "nome_do_banco",
 });
 
