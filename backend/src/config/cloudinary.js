5315970123c6a23ada3121e8057bae45f6bd1eb5
GiovanniAzevedo_DEV <giovanniazevedo065@gmail.com>
2026-02-12 21:21:49 -0300

    teste 2

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -6,7 +6,8 @@ import { v2 as cloudinary } from 'cloudinary';
     cloudinary.config({ 
         cloud_name: process.env.CLOUD_NAME , 
         api_key: process.env.API_KEY, 
-        api_secret: process.env.API_SECRET // Click 'View API Keys' above to copy your API secret
+        api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
+        api_url: process.env.URL_API
     });
     
    export default cloudinary
